a532f120a351064d9f3cf4f5667fe2ccd82ccb2b
rubayet170746 <rafirubayet75@gmail.com>
2020-06-21 23:27:19 +0600

    added 48

---
+++ 48. Rotate Image.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int N = matrix.size();
+        for(int i=0;i<N;i++)
+        {
+            for(int j=i;j<N;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<(N/2);j++)
+            {
+              int temp = matrix[i][j];
+              matrix[i][j]=matrix[i][N-1-j];
+              matrix[i][N-1-j]=temp;
+              
+            }
+        }
+        
+    }
+};
