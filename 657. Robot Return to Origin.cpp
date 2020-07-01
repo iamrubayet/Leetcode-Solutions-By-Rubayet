a23f0bb63fd86e6e29f06b9f85abe206ce770058
rubayet170746 <rafirubayet75@gmail.com>
2020-07-01 21:32:37 +0600

    added 657

---
+++ 657. Robot Return to Origin.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0;
+        int y=0;
+        for(int i=0;i<moves.size();i++)
+        {
+          if(moves[i]=='U')
+          {
+              y++;
+          }
+            else if(moves[i]=='D')
+            {
+               y--; 
+            }
+            else if(moves[i]=='R')
+            {
+                x++;
+            }
+            else if(moves[i]=='L'){
+                x--;
+            }
+        
+        }
+        if(x==0 && y==0)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+        
+    }
+};
