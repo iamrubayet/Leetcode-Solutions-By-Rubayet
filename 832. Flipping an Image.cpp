0dd754a4387e5a7f2216bdc5cbebca4a00f6aab6
rubayet170746 <rafirubayet75@gmail.com>
2020-07-02 21:39:31 +0600

    added 832

---
+++ 832. Flipping an Image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        for(int i=0;i<A.size();i++)
+        {
+          int j=0;
+          int k=A[i].size()-1;
+            while(j<k)
+            {
+                int temp = A[i][j];
+                A[i][j++]=A[i][k];
+                A[i][k--]=temp;
+            }
+            
+            for(int j=0;j<A[i].size();j++)
+            {
+                A[i][j]=A[i][j]==1 ? 0 : 1;
+            }
+        }
+        return A;
+        
+    }
+};
