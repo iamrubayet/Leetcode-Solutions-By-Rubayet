912691802bb1d6d5ba952f1d2d626d14c359e7f1
rubayet170746 <rafirubayet75@gmail.com>
2020-07-04 23:31:21 +0600

    added more\

---
+++ 1389. Create Target Array in the Given Order.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> result;
+        for(int i=0;i<index.size();i++)
+        {
+            result.insert(result.begin()+index[i],nums[i]);
+        }
+        return result;
+        
+    }
+};
