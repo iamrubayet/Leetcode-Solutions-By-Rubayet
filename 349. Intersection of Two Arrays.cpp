af0099be97db728acd74c6568e705f25c3e4ce95
rubayet170746 <rafirubayet75@gmail.com>
2020-07-09 19:36:38 +0600

    added 349

---
+++ 349. Intersection of Two Arrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_set<int> m(nums1.begin(),nums1.end());
+        for(auto a:nums2)
+        {
+          if(m.count(a)) 
+          {
+            res.push_back(a);
+            m.erase(a);
+          } 
+        }
+        return res;
+        
+    }
+};
