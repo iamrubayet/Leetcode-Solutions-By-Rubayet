c9b495edb5585f4f550c52bcd14887d773b0638f
rubayet170746 <rafirubayet75@gmail.com>
2020-07-28 01:56:25 +0600

    update

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -14,3 +14,30 @@ public:
         
     }
 };
+
+
+
+//another 
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            int flag=0;
+            for(int j=0;j<nums.size()-i-1;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    swap(nums[j],nums[j+1]);
+                    flag =1;
+                }
+            }
+            if(flag==0)
+            {
+                break;
+            }
+        }
+        
+    }
+};
