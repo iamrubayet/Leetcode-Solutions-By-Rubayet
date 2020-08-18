357140f8fdf18195796e17f2789f118218b3af36
rubayet170746 <rafirubayet75@gmail.com>
2020-08-18 21:51:16 +0600

    added two solutions

---
+++ 67. Add Binary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result;
+        
+        int carry = 0;
+        int i = a.size()-1;
+        int j = b.size()-1;
+        while(i>=0 || j>=0 )
+        {
+            int sum = carry;
+            
+            if(i>=0) sum += a[i--]-'0';
+            if(j>=0) sum += b[j--]-'0';
+            carry = sum > 1 ? 1:0;
+            result += to_string(sum%2);
+        }
+        
+        if(carry) result += to_string(carry);
+        reverse(result.begin(),result.end());
+        return result;
+        
+        
+    }
+};
