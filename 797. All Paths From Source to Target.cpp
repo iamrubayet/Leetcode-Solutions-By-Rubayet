253a6ff7671741ffa852f804d215963c775ed74f
rubayet170746 <rafirubayet75@gmail.com>
2020-09-22 01:12:10 +0600

    added 797

---
+++ 797. All Paths From Source to Target.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+         vector<vector<int>> result;
+        
+        vector<int> path;
+        
+        dfs(graph,result,path,0);
+        return result;
+        
+         
+        
+    }
+    
+    void dfs(vector<vector<int>>& graph,vector<vector<int>>& result,vector<int> path,int u)
+    {
+        path.push_back(u);
+        if(u==graph.size()-1) result.push_back(path);
+        else
+            for(int v : graph[u]) dfs(graph,result,path,v);
+        path.pop_back();
+    }
+};
