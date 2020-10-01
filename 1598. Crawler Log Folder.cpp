d9e411ce527bd38afd097fff0c571289593bbe48
rubayet170746 <rafirubayet75@gmail.com>
2020-10-01 23:23:35 +0600

    added string problems

---
+++ 1598. Crawler Log Folder.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int steps = 0;
+        for(auto log : logs )
+        {
+            if(log == "../")
+            {
+                steps = max(0,steps-1);
+            }
+           else if(log !="./")
+            {
+                steps++;
+            }
+        }
+        return steps;
+        
+    }
+};
