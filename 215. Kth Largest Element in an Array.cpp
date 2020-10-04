457d352d8a939c277f517cfe29507e134e7ecaa8
rubayet170746 <rafirubayet75@gmail.com>
2020-10-04 22:30:00 +0600

    added 25th medium

---
+++ 215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-k];
+        
+    }
+};
+
+
+//2nd way
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(int i:nums)
+        {
+            pq.push(i);
+            if(pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+        return pq.top();
+        
+    }
+};
