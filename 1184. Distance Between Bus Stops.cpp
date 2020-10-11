9cbbe9769e811fea637ee412516731e4271ba996
rubayet170746 <rafirubayet75@gmail.com>
2020-10-11 19:48:37 +0600

    added 1184

---
+++ 1184. Distance Between Bus Stops.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
+        int sum1 = 0;
+        int sum2 = 0;
+        
+        if(start > destination)
+        {
+            swap(start,destination);
+        }
+        
+        for(int i=0;i<distance.size();i++)
+        {
+            if(i >= start && i < destination)
+            {
+                sum1 += distance[i];
+            }
+            else
+            {
+                sum2 += distance[i];
+            }
+        }
+        return min(sum1,sum2);
+        
+    }
+};
