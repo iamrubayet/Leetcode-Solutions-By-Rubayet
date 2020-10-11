b373cea3e8e48b23824550bede8fb4f21b678e85
rubayet170746 <rafirubayet75@gmail.com>
2020-10-11 21:55:53 +0600

    added 1089'

---
+++ 1089. Duplicate Zeros.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        queue<int> q;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            q.push(arr[i]);
+            if(arr[i]==0)
+            {
+                q.push(0);
+            }
+            arr[i]=q.front();
+            q.pop();
+        }
+        
+    }
+};
