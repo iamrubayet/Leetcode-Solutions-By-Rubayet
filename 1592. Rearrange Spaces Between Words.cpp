fc21ab4a3cfefd7adb4f8d1dbfbd2fe94fe6a6ed
rubayet170746 <rafirubayet75@gmail.com>
2020-10-12 20:26:58 +0600

    300th problem solved

---
+++ 1592. Rearrange Spaces Between Words.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reorderSpaces(string text) {
+    int spaces = 0;
+    string word, s;
+    vector<string> words;
+    for (auto ch : text) {
+        if (ch == ' ') {
+            ++spaces;
+            if (!word.empty())
+                words.push_back(word);
+            word = "";
+        }
+        else
+            word += ch;
+    }
+    if (!word.empty())
+        words.push_back(word);
+    if (words.size() == 1)
+        return words.back() + string(spaces, ' ');
+    int gap = spaces / (words.size() - 1), suff = spaces % (words.size() - 1);
+    for (auto w : words)
+        s += w + string(gap, ' ');
+    return s.substr(0, s.size() - gap) + string(suff, ' ');
+}
+};
