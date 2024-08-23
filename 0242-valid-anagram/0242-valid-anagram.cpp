858415e741ecf64d65969afc44b0318e2314b96e
Shahriar Rahman Rubayet <rubayetaivrda@gmail.com>
2024-08-23 19:33:11 +0600

    Time: 5 ms (82.00%), Space: 10 MB (23.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() !=  t.size())
+        if (s.size() != t.size())
         {
             return false;
         }
         
-        unordered_map<char,int> smap;
-        unordered_map<char,int> tmap;
+        unordered_map<char,int> charfreq;
         
         for(int i=0;i<s.size();i++)
         {
-            smap[s[i]]++;
-            tmap[t[i]]++;
-            
+            charfreq[s[i]]++;
+            charfreq[t[i]]--;
             
         }
         
-        for(int i=0;i<smap.size();i++)
+        for(const auto& count : charfreq)
         {
-            if(smap[i] != tmap[i])
+            if ( count.second != 0)
             {
                 return false;
-                
             }
         }
         return true;
