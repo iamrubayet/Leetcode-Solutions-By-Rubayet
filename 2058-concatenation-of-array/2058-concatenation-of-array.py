3e4f53b6dcea8ee92f00c785c731f1493b017ebb
Shahriar Rahman Rubayet <rubayetaivrda@gmail.com>
2025-01-03 22:32:58 +0600

    Time: 0 ms (100%), Space: 12.6 MB (39.05%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(2):
+            for num in nums:
+                ans.append(num)
+        return ans
+        
