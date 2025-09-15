ebb43a535dc6a6e6d87a1c6c73c84be9e62dc578
Shahriar Rahman Rubayet <rubayetaivrda@gmail.com>
2025-09-15 13:46:16 +0600

    Time: 0 ms (100%), Space: 19 MB (19.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,55 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> countmap;
-        for(int i=0;i<nums.size();i++)
+        // unordered_map<int,int> countmap;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     countmap[nums[i]]++;
+        // }
+
+        // vector<pair<int,int>> sorted;
+        // for(auto itr=countmap.begin();itr!=countmap.end();++itr)
+        // {
+        //     sorted.push_back({itr->second,itr->first});
+        // }
+
+        // sort(sorted.rbegin(),sorted.rend());
+
+        // vector<int> res;
+        // for(int i=0;i<k;++i)
+        // {
+        //     res.push_back(sorted[i].second);
+        // }
+        // return res;
+
+        unordered_map<int,int> count;
+        int n = nums.size();
+
+        for(int i=0;i<n;i++)
         {
-            countmap[nums[i]]++;
+            count[nums[i]]++;
         }
 
-        vector<pair<int,int>> sorted;
-        for(auto itr=countmap.begin();itr!=countmap.end();++itr)
+
+        vector<pair<int,int>> revcount;
+
+        for(auto itr=count.begin();itr!=count.end();itr++)
         {
-            sorted.push_back({itr->second,itr->first});
+            revcount.push_back({itr->second,itr->first});
         }
 
-        sort(sorted.rbegin(),sorted.rend());
+
+        sort(revcount.rbegin(),revcount.rend());
 
         vector<int> res;
-        for(int i=0;i<k;++i)
+
+        for(int i=0;i<k;i++)
         {
-            res.push_back(sorted[i].second);
+            res.push_back(revcount[i].second);
         }
+
         return res;
+
 
 
     }
