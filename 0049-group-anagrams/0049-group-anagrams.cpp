86989779e201b977c2b91250288d6105cea37499
Shahriar Rahman Rubayet <rubayetaivrda@gmail.com>
2025-09-15 13:27:54 +0600

    Time: 14 ms (75.78%), Space: 25 MB (76.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> stringmp;
-        int n = strs.size();
+        // unordered_map<string,vector<string>> stringmp;
+        // int n = strs.size();
+        // string temp;
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     temp=strs[i];
+        //     sort(strs[i].begin(),strs[i].end());
+        //     stringmp[strs[i]].push_back(temp);
+
+        // }
+
+        // vector<vector<string>> result;
+
+        // for(auto itr=stringmp.begin();itr!=stringmp.end();++itr)
+        // {
+        //     result.push_back(itr->second);
+
+        // }
+
+        // return result;
+
+        unordered_map<string,vector<string>> stringmap;
+        int n=strs.size();
         string temp;
 
         for(int i=0;i<n;i++)
         {
             temp=strs[i];
             sort(strs[i].begin(),strs[i].end());
-            stringmp[strs[i]].push_back(temp);
-
+            stringmap[strs[i]].push_back(temp);
         }
 
-        vector<vector<string>> result;
+        vector<vector<string>> res;
 
-        for(auto itr=stringmp.begin();itr!=stringmp.end();++itr)
+        for(auto itr=stringmap.begin();itr!=stringmap.end();itr++)
         {
-            result.push_back(itr->second);
+            res.push_back(itr->second);
 
         }
 
-        return result;
+
+        return res;
 
 
         
