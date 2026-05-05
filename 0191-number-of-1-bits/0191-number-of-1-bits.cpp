67e1ef48df1e80fdee1275621a9e93c7f43f352b
Shahriar Rahman Rubayet <rubayetaivrda@gmail.com>
2026-05-06 01:05:01 +0600

    Time: 0 ms (100%), Space: 8.3 MB (46.8%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hammingWeight(int n) {
         int result=0;
-        while(n > 0)
+        while(n)
         {
             result += n % 2;
             n = n/2;
